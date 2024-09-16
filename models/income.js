326b2047bd9662049c205a466c9d6e50588f660c
Rich Bozek <152832616+rbozek@users.noreply.github.com>
2024-09-16 19:09:29 -0400

    add Date

--- models/income.js
+++ models/income.js
@@ -10,10 +10,15 @@ const incomeSchema = new Schema(
     },
     amount: {
       type: Number,
-      //required or no?
+      //required when creating, or no?
       required: true,
     },
-    category: {
+    date: {
+      type: Date,
+      //required when creating, or no?
+      required: true,
+    },
+    frequency: {
       type: String,
       required: true,
       enum: ['News', 'Sports', 'Games', 'Movies', 'Music', 'Television'],
